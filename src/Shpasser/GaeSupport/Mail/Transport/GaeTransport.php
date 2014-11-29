236bfd06a2620193e93d15ed46738f46aa05fe36
Ron Shpasser <shpasser@gmail.com>
2014-11-29 19:37:30 +0000

    Added support for mail attachments

--- src/Shpasser/GaeSupport/Mail/Transport/GaeTransport.php
+++ src/Shpasser/GaeSupport/Mail/Transport/GaeTransport.php
@@ -3,11 +3,13 @@
 use Swift_Transport;
 use Swift_Mime_Message;
 use Swift_Events_EventListener;
+use Swift_Attachment;
 
 use Shpasser\GaeSupport\Foundation\Application;
 
 require_once 'google/appengine/api/mail/Message.php';
 use google\appengine\api\mail\Message as GAEMessage;
+
 
 class GaeTransport implements Swift_Transport {
 
@@ -75,7 +77,10 @@ class GaeTransport implements Swift_Transport {
 			if ($bcc !== '') 		$mail_options['bcc'] = $bcc;
 			if ($replyto !== '')	$mail_options['replyto'] = $replyto;
 
-            $gae_message = new GAEMessage($mail_options);
+			$attachments = $this->getAttachmentsArray($message);
+			if (count($attachments) > 0) $mail_options['attachments'] = $attachments;
+
+			$gae_message = new GAEMessage($mail_options);
 			$gae_message->send();
         } catch (InvalidArgumentException $e) {
             syslog(LOG_WARNING, "Exception sending mail: " . $e);
@@ -88,6 +93,32 @@ class GaeTransport implements Swift_Transport {
 	public function registerPlugin(Swift_Events_EventListener $plugin)
 	{
 		//
+	}
+
+	/**
+	 * Returns an array of attachments extracted from
+	 * a given mail message.
+	 *
+	 * @param $message
+	 *
+	 * @return array
+	 */
+	protected  function getAttachmentsArray($message)
+	{
+		$attachments = array();
+
+		foreach ($message->getChildren() as $entity) {
+			if ($entity instanceof Swift_Attachment)
+			{
+				$attachments[] = array(
+					'name' => $entity->getFilename(),
+					'data' => $entity->getBody(),
+					'content_id' => "<{$entity->getContentType()}>"
+				);
+			}
+		}
+
+		return $attachments;
 	}
 
 }
